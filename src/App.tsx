03d3a24ce701047484db38c4e8fee6831cc7f4d0
Abhijeet Kanwar <kanwar.abhijeet10@gmail.com>
2025-02-11 18:02:33 +0530

    huskey initalised

--- src/App.tsx
+++ src/App.tsx
@@ -19,9 +19,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <Button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </Button>
+        <Button onClick={() => setCount((count) => count + 1)}>count is {count}</Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
