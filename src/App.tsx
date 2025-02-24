ca6f2673fbf40215fc2fc25a1848c469bae5f907
Abhijeet Kanwar <kanwar.abhijeet10@gmail.com>
2025-02-24 16:43:20 +0530

    test

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,7 @@ import { Button } from "./components/ui/button";
 function App() {
   const [count, setCount] = useState(0);
 
-  const hello = () => {
-    console.log("hello");
-  };
+   
 
   return (
     <>
